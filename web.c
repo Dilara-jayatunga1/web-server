0965d6173888ca1dda6c42e784c1c57ea9da09ac
dilara-jayatunga <thirandijayatunga@gmail.com>
2025-01-29 11:51:09 +0530

    Add initial implementation of file handling functions and web server setup

---
+++ web.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<fcntl.h>
+
+#define PORT 8085
+
+int main(){
+    
+}
