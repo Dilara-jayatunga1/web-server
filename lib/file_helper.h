d46d16796f24cb98c2a18def3992b06edaaef0d0
dilara-jayatunga <thirandijayatunga@gmail.com>
2025-02-12 19:40:42 +0530

    Add header file for file helper functions

---
+++ lib/file_helper.h
@@ -0,0 +1,7 @@
+#ifndef FILE_HELPERS_H
+#define FILE_HELPERS_H
+
+int get_file_descriptor(const char *directory, const char *file_name);
+const char *get_file_extension(const char *file_name); 
+
+#endif
