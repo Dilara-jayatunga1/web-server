96956eb3e181c5ee6be83d4f3c9efd8367555832
dilara-jayatunga <thirandijayatunga@gmail.com>
2025-02-15 05:53:01 +0530

    Add MIME type retrieval and URL decoding functions

--- lib/file_helper.h
+++ lib/file_helper.h
@@ -2,6 +2,8 @@
 #define FILE_HELPERS_H
 
 int get_file_descriptor(const char *directory, const char *file_name);
-const char *get_file_extension(const char *file_name); 
+const char *get_file_extension(const char *file_name);
+const char *get_mime_type(const char *file_ext);  // Add the declaration for get_mime_type
+char *url_decode(const char *src); 
 
 #endif
