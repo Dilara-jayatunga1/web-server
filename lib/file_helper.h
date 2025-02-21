e20111b242dba609681125b02fe0695e440f3b10
dilara-jayatunga <thirandijayatunga@gmail.com>
2025-02-21 10:25:39 +0530

    Remove unused file helper functions and related files

--- lib/file_helper.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_HELPERS_H
-#define FILE_HELPERS_H
-
-int get_file_descriptor(const char *directory, const char *file_name);
-const char *get_file_extension(const char *file_name);
-const char *get_mime_type(const char *file_ext);  // Add the declaration for get_mime_type
-char *url_decode(const char *src); 
-
-#endif
